cc1ed6209e902654e433e1b2116cda254e339399
Greg Albers <galbers@getty.edu>
2023-06-30 12:32:25 -0700

    Update public, deployed files to latest

--- public/assets/js/build-epub.js
+++
@@ -1,9 +0,0 @@
-var Peepub   = require('pe-epub-fs')(require('pe-epub'));
-var epubJson = require('/Users/galbers/GitHub/keepitmoving/public/epub.json'); // see examples/example.json for the specs
-var myPeepub = new Peepub(epubJson);
-
-myPeepub.create('/Users/galbers/GitHub/keepitmoving/static/downloads/keepitmoving.epub')
-  .then(function(filePath){
-  console.log(filePath); // the same path to your epub file!
-});
-
